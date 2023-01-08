652105945fd815202474fa8a19dda4cd85640055
KiranmaiPasupuleti <pasupuletikiranmai1@gmail.com>
2023-01-08 04:42:19 +0000

    first Commit

---
+++ src/components/AlertNotifications/index.js
@@ -0,0 +1,61 @@
+// Write your code here
+import {AiFillCheckCircle} from 'react-icons/ai'
+import {RiErrorWarningFill} from 'react-icons/ri'
+import {MdWarning} from 'react-icons/md'
+import {MdInfo} from 'react-icons/md'
+import Notification from '../Notification'
+
+import './index.css'
+
+const AlertNotifications = () => {
+  return (
+    <div className="app-container">
+      <h1>Alert Notifications</h1>
+      <Notification>
+        <div className="each-container">
+          <div className="status">
+            <AiFillCheckCircle className="success" />
+            <h1 className="success">Success</h1>
+          </div>
+          <p className="para">You can access all the files in the folder</p>
+        </div>
+      </Notification>
+
+      <Notification>
+        <div className="each-container">
+          <div className="status">
+            <RiErrorWarningFill className="error" />
+            <h1 className="error">Error</h1>
+          </div>
+          <p className="para">
+            Sorry, you are not authorized to have access to delete the file
+          </p>
+        </div>
+      </Notification>
+
+      <Notification>
+        <div className="each-container">
+          <div className="status">
+            <MdWarning className="warning" />
+            <h1 className="warning">Warning</h1>
+          </div>
+          <p className="para">
+            viewers of this file can see comments and suggestions
+          </p>
+        </div>
+      </Notification>
+
+      <Notification>
+        <div className="each-container">
+          <div className="status">
+            <MdInfo className="info" />
+            <h1 className="info">Info</h1>
+          </div>
+          <p className="para">Anyone on the internet can view these files</p>
+        </div>
+      </Notification>
+    </div>
+  )
+}
+
+export default AlertNotifications
